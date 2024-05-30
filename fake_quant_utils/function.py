002b9c7414e45efd77bbe4965cf0cb48f920e226
zukang.xu <zukang.xu@gitlab.com>
2024-05-31 01:32:19 +0800

    5.31

--- fake_quant_utils/function.py
+++ fake_quant_utils/function.py
@@ -1,56 +1,52 @@
 import torch
 
 class QuantizedMatMul:
-    def __init__(self, num_bits):
+    def __init__(self, num_bits=8, quant_mode="per_tensor"):
         self.num_bits = num_bits
+        self.quant_mode = quant_mode
         
-    def quantize_weight_per_channel_absmax(self, w):
+    def quantize_per_channel_absmax(self, w, num_bits=8):
         # w: (out_features, in_features)
         scales = w.abs().max(dim=-1, keepdim=True)[0]
-        q_max = 2 ** (self.num_bits - 1) - 1
+        q_max = 2 ** (num_bits - 1) - 1
         scales.clamp_(min=1e-5).div_(q_max)
         w.div_(scales).round_().mul_(scales)
         return w
 
+    def quantize_per_token_absmax(self,t, num_bits=8):
+        t_shape = t.shape
+        t.reshape(-1, t_shape[-1])
+        scales = t.abs().max(dim=-1, keepdim=True)[0]
+        q_max = 2 ** (num_bits - 1) - 1
+        scales.clamp_(min=1e-5).div_(q_max)
+        t.div_(scales).round_().mul_(scales)
+        return t
 
-    @torch.no_grad()
-    def quantize_weight_per_tensor_absmax(self, w):
+    # @torch.no_grad()
+    def quantize_per_tensor_absmax(self, w, num_bits=8):
         # w: (out_features, in_features)
         scales = w.abs().max()
-        q_max = 2 ** (self.num_bits - 1) - 1
+        q_max = 2 ** (num_bits - 1) - 1
         scales.clamp_(min=1e-5).div_(q_max)
         w.div_(scales).round_().mul_(scales)
         return w
 
     def __call__(self, A, B):
-        A_q = self.quantize_weight_per_tensor_absmax(A)
-        B_q = self.quantize_weight_per_tensor_absmax(B)
+        if self.quant_mode == "per_channel":
+            A_q = self.quantize_per_channel_absmax(A,self.num_bits)
+            B_q = self.quantize_per_channel_absmax(B,self.num_bits)
+        elif self.quant_mode == "per_tensor":
+            A_q = self.quantize_per_tensor_absmax(A,self.num_bits)
+            B_q = self.quantize_per_tensor_absmax(B,self.num_bits)
+        elif self.quant_mode == "per_token":
+            A_q = self.quantize_per_token_absmax(A,self.num_bits)
+            B_q = self.quantize_per_token_absmax(B,self.num_bits)
+        else:
+            raise ValueError("Unsupported quantization mode: {}".format(self.quant_mode))
+
         result_q = A_q@B_q
         return result_q
 
-def quantize_weight_per_channel_absmax(w,num_bits):
-    # w: (out_features, in_features)
-    scales = w.abs().max(dim=-1, keepdim=True)[0]
-    q_max = 2 ** (num_bits - 1) - 1
-    scales.clamp_(min=1e-5).div_(q_max)
-    w.div_(scales).round_().mul_(scales)
-    return w
-
-
-@torch.no_grad()
-def quantize_weight_per_tensor_absmax(w,num_bits):
-    # w: (out_features, in_features)
-    scales = w.abs().max()
-    q_max = 2 ** (num_bits - 1) - 1
-    scales.clamp_(min=1e-5).div_(q_max)
-    w.div_(scales).round_().mul_(scales)
-    return w
-
-def Qmatmul( A, B):
-    A_q = quantize_weight_per_tensor_absmax(A,8)
-    B_q = quantize_weight_per_tensor_absmax(B,8)
-    result_q = torch.matmul(A_q, B_q)
-    return result_q
 
 if __name__ == "__main__":
     A = torch.randn(2, 3)
